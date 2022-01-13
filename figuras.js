a350b8544b2a7945d94b64da5a289399ab3051db
easyclass1 <97700049+easyclass1@users.noreply.github.com>
2022-01-13 14:37:42 -0500

    Add files via upload

---
+++ figuras.js
@@ -0,0 +1,14 @@
+
+function calcularPerimetroCuadrado() 
+{
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+  if(value=="ZH5K2")
+  {
+    alert("El codigo de descuento es correcto. Escribenos a nuestro Wpp:  https://bit.ly/Whatsapp1easyclass");
+  }
+  else
+  {
+    alert("El codigo no es correcto :( intenta de nuevo :)")
+  }
+}
